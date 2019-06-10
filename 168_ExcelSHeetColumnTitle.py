9ccf2cb0d09dac3e06b94821acb55a8fcab726a2
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-10 02:10:32 -0400

    currently only one method

---
+++ 168_ExcelSHeetColumnTitle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        result = ""
+        while n  > 0:
+            if n % 26 == 0:                
+                result = 'Z' + result
+                n = int(n/26) - 1
+            else: 
+                # find the character for reminder
+                result = chr(n % 26+64) + result
+                # find the times of full loop, to find the tenth digit num
+                n = int(n/26)
+        return result
