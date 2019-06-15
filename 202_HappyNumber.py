1f4715b8f2ca68806bd8b14349b58c56a1e38b3a
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-15 18:15:37 -0400

    my method

---
+++ 202_HappyNumber.py
@@ -0,0 +1,20 @@
+class Solution:
+	#my method 32ms, 13.2 MB (98.67%+ 34.83%)
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+        elif n == 0:
+            return False
+        
+        collect = set()
+        n_str = str(n)
+        summ = n
+        
+        while summ not in collect:
+            collect.add(summ)
+            n_str = str(n)
+            summ = sum([int(i)**2 for i in n_str])
+            if summ == 1:
+                return True
+            n = summ
+        return False
