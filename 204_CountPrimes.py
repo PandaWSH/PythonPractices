798cfd766a717e7b0883dba61cd791a67fa6bcee
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-15 20:44:09 -0400

    Sieve of Eratosthenes Method

---
+++ 204_CountPrimes.py
@@ -0,0 +1,37 @@
+# my method 1, works, but time exceeds
+class Solution:
+    def countPrimes(self, n: int) -> int:
+
+        if n <= 2: #smaller than 2
+            return 0
+        else: #greater than 2
+            counter = 1 #at least one
+            i = 3
+            #determine prime number
+            while i < n:
+                counter = counter + self.findprime(i)
+                i+=1
+        return counter
+                
+    def findprime(self, m):
+        for i in range(2,m):
+            if m % i == 0:
+                return 0
+        return 1
+
+# scientific method Sieve of Eratosthenes
+def countPrimes(self, n: int) -> int:
+        if n < 3:
+            return 0     
+        else:
+            # 首先生成了一个全部为1的列表
+            output = [1] * n
+            # 因为0和1不是质数,所以列表的前两个位置赋值为0
+            output[0],output[1] = 0,0
+             # 此时从index = 2开始遍历,output[2]==1,即表明第一个质数为2,然后将2的倍数对应的索引
+             # 全部赋值为0. 此时output[3] == 1,即表明下一个质数为3,同样划去3的倍数.以此类推.
+            for i in range(2,int(n**0.5)+1): 
+                if output[i] == 1:
+                    output[i*i:n:i] = [0] * len(output[i*i:n:i])
+         # 最后output中的数字1表明该位置上的索引数为质数,然后求和即可.
+        return sum(output)
