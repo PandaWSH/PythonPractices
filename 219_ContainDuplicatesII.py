16467681c838bc173c34bb0d009e387ade69084d
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-16 01:31:55 -0400

    suspect the auto-grader flaw

---
+++ 219_ContainDuplicatesII.py
@@ -0,0 +1,18 @@
+# suspect the auto-grader with flaw, [99,99], 2 returns True????
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        #check exist
+        if not nums:
+            return False
+        if len(nums) == 1:
+            return False
+        if len(nums) <= k:
+            return False
+        i = 0
+        while i < len(nums)-k:
+            cur = nums[i]
+            for j in range(i+1,i+k+1):
+                if nums[j] == cur:
+                    return True
+            i+=1
+        return False
