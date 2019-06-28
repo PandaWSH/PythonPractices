618dc8c253bac3fa3a562a0e61111fbd0061a6e0
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-28 01:36:09 -0400

    second time, additional two methods

--- 14_LongestCommonPrefix.py
+++ 14_LongestCommonPrefix.py
@@ -25,29 +25,55 @@ while the second one is less than 18.83%
                     break
             return result
 
-    # inspired online 91.89% method, improved to 97.53% fast
+    # inspired online 91.89% method, improved to 99.94% fast (24ms + 13.2 MB [44.50%])
     def longestCommonPrefix(self, strs):
-        #result output
-        ret = ""
-
-        #empty string
-        if len(strs) == 0:
-            return ret
+        # empty strs
+        if not strs: return "" 
 
         # the length of min sub-string
-        i = len(min(strs, key=len))
+        l = len(min(strs, key=len))
         #modify the string: cut all string in the list to the shortest length
-        #works but ocupy additional space??
-        strs= [s[:i] for s in strs]
+        strs= [s[:l] for s in strs]
 
         # while no empty string
         # --- recursive ---
         while len(min(strs, key=len)) > 0: # when there's no empty string
             # base line (break): when all same string, set() convert the list to single string list
             if len(set(strs)) == 1: 
-                ret = strs[0]
-                break
+                return strs[0]
             else:
                 # otherwise, remove the last character from each string
-                strs = [s[:-1] for s in strs]
-        return ret
+                strs = [ s[:-1] for s in strs]
+        return ""
+
+
+    # method 3, 28ms + 13.4MB ( 99.33% + 10.07%) 借助ASCII的的default排序，只用比较最大最小的common
+    def longestCommonPrefix(self, strs):
+        if not strs: return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i,x in enumerate(s1):
+            if x != s2[i]:
+                return s2[:i]
+        return s1
+
+    # method 4, 36ms + 13.4MB (86.7% + 5.6%)
+    # zip(*list), set, 
+    def longestCommonPrefix(self, strs):
+        if not strs: return ""
+        
+        new_strs = list(zip(*strs))
+        if len(new_strs) == 0:
+            return ""
+        l = len(new_strs)
+        result = "" 
+        
+        for i in range(l):
+            if len(set(new_strs[i])) == 1:
+                result += new_strs[i][0]
+            else:
+                break
+        return result
+            
+                
+                
