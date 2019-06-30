bbc9cd0825cba87c00d289acade81e9092445a82
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-30 14:48:09 -0400

    second time - oneline command could use more space and run slowlier

--- 67_AddBinary.py
+++ 67_AddBinary.py
@@ -1,6 +1,13 @@
 class Solution:
+	# method 1: 36ms + 13MB
     def addBinary(self, a: str, b: str) -> str:
         #convert from input string to decimal, then sum up
         result = int(a,2) + int(b,2)
         #convert the decimal result to binary string, remove '0b'
         return str(bin(result))[2::]
+
+
+    # second time - same method but one line, slower and more space
+    # 52ms + 13.2 MB
+    def addBinary(self, a: str, b: str) -> str:
+    	return str(bin(int(a,2) + int(b,2))[2:])
