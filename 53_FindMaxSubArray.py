da86489663570459d8f7ee64f3294e39a93effd3
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-30 14:06:00 -0400

    second time - modifited one previous method, speed up and use fewer space

--- 53_FindMaxSubArray.py
+++ 53_FindMaxSubArray.py
@@ -15,6 +15,25 @@ class Solution(object):
             	summ=0
         return maxsofar
 
+    # second time - similar to method one, but use while-loop occupying fewer space
+    # 36ms + 13.5MB (97.50% + 85.66%)
+    class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # initialize
+        summ,i = 0,0
+        currentmax = max(nums)
+        # single element list
+        if len(nums) == 1:
+            return nums[0]
+        while i < len(nums):
+            summ += nums[i]
+            if summ < 0:
+                summ = 0
+            else:
+                currentmax = max(currentmax,summ)
+            i += 1
+        return currentmax
+
     # 94.66% + 68.11%
     def maxSubArray2(self, nums): 
     	ret=float('-inf')
@@ -23,7 +42,8 @@ class Solution(object):
         
         for i in range(1,len(nums)):
             # exclude the first element
-            prevsum=max(nums[i],nums[i]+prevsum) #这一步和method1的比较summ是否>1道理一样，
+            prevsum=max(nums[i],nums[i]+prevsum) 
+            # 这一步和method1的比较summ是否>1道理一样
             # 是为了看这一位数有没有把sum拉低到负数，如果没有，就不会重新开始计数
             # 为什么拉低到负数就要重新开始计数呢？--相当于让后一个数输在起跑线上，还不如让它从0开始
             
