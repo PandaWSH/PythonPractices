f59ebc1ee1fc4e6b4a5b999f78e161f518342424
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-07-04 18:11:48 -0400

    11 second time - same method

--- Medium/11_ContainerWIthMostWater.py
+++ Medium/11_ContainerWIthMostWater.py
@@ -14,3 +14,21 @@ class Solution:
                 end -=1
                 
         return maxArea
+
+    # second time - same
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        curmax = 0
+        
+        while left < right:
+            width = right - left
+            cur = width * min(height[left],height[right])
+            curmax  = max(cur,curmax)
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -=1
+                
+        return curmax
