73bfb0dd4bb4db02d46bc5dc33edb06475bb63b1
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-07-09 21:45:15 -0400

    video to image

---
+++ OpenCV/VideoToImage.py
@@ -0,0 +1,17 @@
+import cv2
+vidcap = cv2.VideoCapture('PLA_210Tex_170Tt_1mmRetract_6mms_Default_Pappas.avi')
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
+    return hasFrames
+sec = 0
+frameRate = 0.5 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
